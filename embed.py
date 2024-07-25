e334acb6bdff721ae114ecb56712ded755a7d592
Rolf <107631389+Rolf-TES@users.noreply.github.com>
2024-07-25 21:11:05 +0200

    embed.py

--- embed.py
+++ embed.py
@@ -1,8 +1,6 @@
-import discord
-from discord.ext import commands
+import discord; from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True
+intents = discord.Intents.default(); intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.command(name='embed')
@@ -12,8 +10,7 @@ async def send_embed(ctx, *, args):
         await ctx.send('Proper syntax: Author | Title | Description | Image URL')
         return
     author, title, description, url = args
-    embed = discord.Embed(title=title, description=description, color=0x080808)
-    embed.set_author(name=author)
-    embed.set_image(url=url)
+    embed = discord.Embed(title=title, description=description, color=0x080808) # Change color here.
+    embed.set_author(name=author); embed.set_image(url=url)
     await ctx.send(embed=embed)
 bot.run('YOUR_BOT_TOKEN')
